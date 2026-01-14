aee0bf046a93e0e66246a02af6dab05db872c4df
nkjcolin <colinngkj254@gmail.com>
2026-01-14 10:29:13 +0800

    update db connection

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -9,7 +9,7 @@ const sql = postgres({
   database: process.env.AZURE_POSTGRESQL_DATABASE,
   username: process.env.AZURE_POSTGRESQL_USER,
   password: process.env.AZURE_POSTGRESQL_PASSWORD,
-  // ssl: 'require',
+  ssl: 'require',
 });
 
 async function seedUsers() {
