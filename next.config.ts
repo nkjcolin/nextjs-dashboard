23445290d7e9a65ee6c25e1be7ba8d4a69b5ffac
nkjcolin <colinngkj254@gmail.com>
2026-01-26 10:15:36 +0800

    Revert "update next config"
    This reverts commit 75b0ee274b49aa29140557fa992c64879bab1d9b.

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        process.env.PUBLIC_IP!,
-        process.env.AZURE_IP!,
-      ],
-    },
-  },
     reactStrictMode: true,
 };
 
